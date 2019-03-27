51c45e9509519ec7145fded9c14443505583d7f2
Matthew Wilson <wilsonmatt219@gmail.com>
2019-03-27 16:08:29 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,3 +15,10 @@ WIN_COMBINATIONS = [
   [2, 4, 6]
 ]
   
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combo|
+    win_index_1 = win_combo[0]
+    win_index_2 = win_combo[1]
+    win_index_3 = win_combo[2]
+
+  
